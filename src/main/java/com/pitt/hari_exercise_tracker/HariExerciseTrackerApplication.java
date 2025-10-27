4f8f63d82c3d8a7930d219b025eeb2abcb6f0cc4
TrentChen99 <trentcyq@163.com>
2025-10-27 10:11:48 -0400

    Setup

---
+++ src/main/java/com/pitt/hari_exercise_tracker/HariExerciseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pitt.hari_exercise_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HariExerciseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HariExerciseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pitt/hari_exercise_tracker/HariExerciseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitt.hari_exercise_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HariExerciseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
